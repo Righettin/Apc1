0e5cbbc198d8d7df0598af38f3ffcbbe73672189
Righettin <142408280+Righettin@users.noreply.github.com>
2023-08-23 13:56:22 +0000

    envio para github

---
+++ Aulas/aula01/meuprograma.c
@@ -0,0 +1,18 @@
+/*************************
+Autor: João Pedro Righetto
+Data:23/08/20233
+
+Programa basico em C
+***************************/
+#include <stdio.h>
+#include <math.h>
+
+// funcao principal
+
+int main() {
+  printf("Olá pessoal!\n");
+  printf("Raiz quadrada de 2 é %f\n", sqrt(2));
+  printf("quero um açai por favor\n");
+  return 0;
+}
+  
