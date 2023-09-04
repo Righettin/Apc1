094546b5f88459ff5411d9bdedab87869c0026ec
Righettin <142408280+Righettin@users.noreply.github.com>
2023-09-04 13:54:27 +0000

    codigo da aula 03

---
+++ Aulas/aula02/constantes.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define PI 3.1415926
+#define SEXO_MASCULINO 1
+
+int main() {
+
+  float angulo = * 45;
+char sexo = SEXO MASCULINO;
+const char STATUS ATIVO = 'a';
+//PI = 3.14;
+  //STATUS_ATIVO
+
+  return 0;
+}
