f0f24fa38162d08ba36f064e7324fa0d937e1278
Righettin <142408280+Righettin@users.noreply.github.com>
2023-09-18 13:49:18 +0000

    aula03

---
+++ Aulas/aula03/notalegal.c
@@ -0,0 +1,19 @@
+#include <stdio.h> // funções scanf e printf
+
+int main() {
+
+  printf("-------------------------------------\n");
+  printf("           NOTA LEGAL                \n");
+  printf("-------------------------------------\n");
+  printf("Item                    Qtd Valor    \n");
+  printf("Caderno                 001     10,00\n");
+  printf("Lapis                   005      1,00\n");
+  printf("Borracha                010      5,00\n");
+  printf("-------------------------------------\n");
+  printf("Total                           65,00\n");
+  printf("-------------------------------------\n");
+
+  return 0;
+}
+  
+  
