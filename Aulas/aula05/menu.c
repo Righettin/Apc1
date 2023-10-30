999a6e5e20a7a3de06ea5ba67123fed0c2c1de0f
Righettin <142408280+Righettin@users.noreply.github.com>
2023-10-30 13:51:04 +0000

    aula06 matrizes

--- Aulas/aula05/menu.c
+++ Aulas/aula05/menu.c
@@ -1,22 +1,82 @@
-#include <stdio.h>
 
-int main () {
-  char opcao = -1;
-  
-  while(1) {
-    printf("MENU PRINCIPAL\n");
-    printf("1 - Consultar saldo\n");
-    printf("2 - Fazer recarga\n");
-    printf("3 - Listar recados\n");
-    printf("0 - Sair\n");
-    printf("Sua escolha => ");
-    int leu_certo = scanf ("%c", &opcao); 
-    getchar();
 
-    switch(opcao) {
-      case '1' : printf("Seu saldo é %.2f\n", saldo); break;
-      case '2' : printf("Escolha um valor de recarga:"); break;
-      case '3' : printf("Voce nao tem recados\n"); break;
-      case '0' : break;
-      default : printf("Opcao invalida\n"); break;
+    #include <stdio.h>
+    #include <stdlib.h>
+
+    int main() {
+      char opcao = 0;
+      float saldo = 0.0f;
+
+      while(1) {
+        int ok = system("clear");
+        printf("MENU PRINCIPAL\n");
+        printf("▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄\n");
+        printf("█  1 - Consultar saldo                                     █\n");
+        printf("█  2 - Fazer recarga                                       █\n");
+        printf("█  3 - Listar recados                                      █\n"); 
+        printf("█  0 - Sair                                                █\n"); 
+        printf("█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█\n");
+        printf("Sua escolha => ");
+        ok = scanf("%c", &opcao);
+        getchar();
+
+        switch(opcao) {
+          case '1': 
+            ok = system("clear");
+            printf("CONSULTA SALDO\n");
+            printf("Seu saldo é %.2f\n", saldo); 
+            printf("Pressione ENTER para continuar...");
+            getchar();
+            break;
+          case '2': {
+            char valor = 0;
+            while (valor != '0') {
+              ok = system("clear");
+              printf("FAZER RECARGA\n");
+              printf("1 - R$ 10,00\n");
+              printf("2 - R$ 20,00\n");
+              printf("5 - R$ 50,00\n");
+              printf("Escolha um valor de recarga ou digite 0 para retornar: ");
+              ok = scanf("%c", &valor);
+              getchar();
+
+              switch(valor) {
+                case '1': saldo = saldo + 10.0f; break;
+                case '2': saldo = saldo + 20.0f; break;
+                case '5': saldo = saldo + 50.0f; break;
+                case '0': break;
+                default: 
+                  printf("Opção inválida!\n"); 
+                  printf("Pressione ENTER para continuar...");
+                  getchar();
+                  break;
+              }
+
+              if (valor == '1' || valor == '2' || valor == '5') {
+                printf("\e[0;32mRecarga realizada com sucesso!\e[0m\n");
+                printf("Pressione ENTER para continuar...");
+                getchar();
+              }
+            }    
+            break;
+          }
+          case '3': 
+            ok = system("clear");
+            printf("LISTAR RECADOS\n"); 
+            printf("Você não tem recados\n"); 
+            printf("Pressione ENTER para continuar...");
+            getchar();
+            break;
+          case '0': break;
+          default: 
+            printf("\e[0;31mOpção inválida!\e[0m\n");
+            printf("Pressione ENTER para continuar...");
+            getchar();
+            break;
+        }
+
+        if (opcao == '0') break;
+      }
+
+      return 0;
     }
---
+++ Aulas/aula06/matrizes.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+int main() {
+  int numeros[10]; // conjunto de 10 numeros
+
+  numeros[0] = 10;
+  numeros[1] = 1;
+  numeros[2] = 2;
+  numeros[3] = 5;
+  numeros[4] = 7;
+  numeros[5] = 0;
+  numeros[6] = 2;
+  numeros[7] = 8;
+  numeros[8] = 6;
+  numeros[9] = 3;
+
+  for(int i=0; i<10; i++) {
+    printf("%d, ", numeros[i]);
+  }
+
+  printf("\n");
+
+  float notas[10][2];
+     // 0  1
+  //0   5  4
+  //1   6  3
+  //2   4  7
+  //3   10  4
+  //4
+  //5
+  //6
+  //7
+  //8
+  //9
+
+notas[0][0]
+notas[1][0]
+notas[2][0]
+notas[3][0]
+notas[4][0]
+notas[5][0]
+notas[5][0]
+notas[6][0]
+notas[7][0]
+notas[8][0]
+notas[9][1]
+notas[0][1]
+notas[1][1]
+notas[2][1]
+notas[3][1]
+notas[4][1]
+notas[5][1]
+notas[6][1]
+notas[7][1]
+notas[8][1]
+
+
+  return 0;
+}
